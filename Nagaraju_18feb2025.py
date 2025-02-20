73f79bbae0c053d23114ce75d2eb79558675c472
Nagarajub1902 <bollaboinanagaraju0714@gmail.com>
2025-02-20 10:04:26 +0530

    Add files via upload

---
+++ Nagaraju_18feb2025.py
@@ -0,0 +1,17 @@
+#Find outputs(H/W)
+a={10:'Ramesh',20:'kiran',15:'Amar',18:'Sita'}
+print(a)
+print(type(a))
+print(a[10])
+print(a[20])
+print(a[15])
+print(a[18])
+print([19])
+print(a[0])
+print(a['Amar'])
+a[15]='krishna'
+del a[20]
+a[25]='vamshi'
+print(a)
+print(len(a))
+print(a*2)
