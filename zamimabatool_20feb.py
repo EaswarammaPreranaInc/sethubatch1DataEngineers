cda0cc7df6b4d4d0d884ab99a7b13c443622de54
ZAMIMA BATOOL <119119931+zamimabatool@users.noreply.github.com>
2025-02-21 09:55:02 +0530

    Create zamimabatool_20feb.py

---
+++ zamimabatool_20feb.py
@@ -0,0 +1,13 @@
+
+#  or  operator  demo program
+print(True  or  False)   #   True
+print(False  or  True)   #  True
+print(True  or  True) #  True
+print(False  or   False)    # False
+print(10  or  20)   #  10
+print(0   or  20)  #   20
+print(-25  or  0) # -25
+print(''  or  35) # 35
+print(6j  or  'Hyd') # 6j
+print(0.0  or  3 + 4j) # 3+4j
+print('Hyd'   or   10.8) # 'Hyd'!
