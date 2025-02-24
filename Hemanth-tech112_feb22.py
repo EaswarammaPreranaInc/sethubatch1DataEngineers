46de92e3105d9299e610b6ddcda7b271734bef99
Hemanth-tech112 <guntukahemanth@gmail.com>
2025-02-24 09:57:04 +0530

    Create Hemanth-tech112_feb22.py
    python homework

---
+++ Hemanth-tech112_feb22.py
@@ -0,0 +1,15 @@
+# Find  outputs
+from keyword import kwlist  #How  to  import   kw  list
+print(kwlist)  #(How  to  print  kwlist) i.e ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+print(len(kwlist)) #  (How  to  print  number  of  keywords) i.e 35
+print(type(kwlist)) #  (How  to  print  type  of kwlist) i.e < class 'list'>
+# print((keyword.kwlist)   # Error because keyword module is not imported
+
+
+
+#  Find  outputs  (Home work)
+import keyword #  (How  to  import   keyword  module)
+print(keyword.kwlist) #  (How  to print kwlist)['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+print(len(keyword.kwlist)) #  (How  to  print  number  of  keywords) i.e. 35
+print(type(keyword.kwlist)) #  (How  to  print  type  of kwlist)     i.e. < class 'list'>
+# print(kwlist) #  Error because kwlist is not in the current module
