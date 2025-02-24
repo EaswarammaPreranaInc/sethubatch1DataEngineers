36583e1554aecceada3d071d550d5e46152659d1
ShirishaG08 <gundelishirishayadav@gmail.com>
2025-02-24 13:26:50 +0530

    Create ShirishaG08feb24.py
    we had learnt "From Module" and  "import Module" by using the keyword module and kwlist is a member of the  keyword module

---
+++ ShirishaG08feb24.py
@@ -0,0 +1,16 @@
+#program 1
+#prgm find outs
+#How Keyword import kwlist
+from Keyword import kwlist
+print(kwlist) #printHow to print kwlist)
+print(len(kwlist)) # How to print No. of Keywords
+print(type(kwlist)) #How to print type of Kwlist
+print(keyword.kwlist) # Error because keyword modulo is not imported
+
+
+#program 2
+import keyword #How to import keyword module
+print(keyword.kwlist) #how to print kwlist
+print(len(keyword.kwlist)) #  How to print No. of keywords
+print(type(keyword.kwlist)) # how to print type of kwlist
+print(kwlist)
