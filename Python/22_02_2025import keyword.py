b8393b90e9d5ac3ea7bd0133a594ebb77b1e4277
minishay <70064233+minishay@users.noreply.github.com>
2025-03-03 09:32:43 +0530

    Add files via upload
    Home work programs till date

---
+++ Python/22_02_2025import keyword.py
@@ -0,0 +1,10 @@
+import keyword  
+print(keyword.kwlist)  # Prints Python's reserved keywords  
+####
+# Print the list of Python keywords
+print(keyword.kwlist) # 
+# Print the number of Python keywords
+print(len(keyword.kwlist)) # 35
+# Print the type of kwlist
+print(type(keyword.kwlist))
+print(keyword.kwlist)
---
+++ Python/Ternary.py
@@ -0,0 +1,14 @@
+# # # print({10 , 20}  |  {30 , 20})
+# # # print({10 : 'Hyd' , 20 : 'Sec'} |  {30 : 'Cyb' , 20 : 'Vja'})
+# # print(set(range(4)) |set(range(5)))
+
+# x = [1 , 2 , 3 , 4]  
+# y = [1 , 2 , 3 , 4]  
+# print(x is y)
+# print(x is not y)
+# print('One'); 
+# print('Two'); 
+# print('Three'); 
+# print('Hyd');
+# print('Sec');
+# print('Cyb');
