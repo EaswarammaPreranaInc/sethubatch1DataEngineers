9fd28f3cc13993a1320dba296f646030e24653ca
Saichandra-9966 <podishettysaichandra@gmail.com>
2025-03-03 09:28:12 +0530

    Create Saichandra_03march.py

---
+++ Saichandra_03march.py
@@ -0,0 +1,9 @@
+for x in {10:20, 30:40, 50:60}.keys():
+    print(x)
+for x in {10:20, 30:40, 50:60}.values():
+    print(x)
+for x in {10:20, 30:40, 50:60}.items():
+    print(x)
+for x in {10:20, 30:40, 50:60}:
+    print(x)
+     
