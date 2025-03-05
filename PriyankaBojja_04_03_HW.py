48c3a02faba3a707d7e3dda612cc0975cf388c3f
priyanka-datapath <198757538+priyanka-datapath@users.noreply.github.com>
2025-03-05 18:23:14 +0530

    Update PriyankaBojja_04_03_HW.py

--- PriyankaBojja_04_03_HW.py
+++ PriyankaBojja_04_03_HW.py
@@ -1,4 +1,4 @@
-'''
+1
 # Find  outputs  (Home  work)
 for  i  in  range(1 , 8):
 	print(i)   
@@ -32,7 +32,8 @@ Sec
 Hello
 else  suite
 Outside  loop
-
+----------------------------------------------------------------------------------------------------------------
+2
 # Find  outputs  (Home  work)
 for  i  in  range(1 , 8):
 	print(i)
@@ -55,7 +56,8 @@ Sec
 Hello
 3
 Outside  loop
-
+-----------------------------------------------------------------------------------------------------------
+3
 # Find  outputs  (Home  work)
 for  i  in  range(1 , 8):
 	print(i)     
@@ -93,43 +95,26 @@ Sec
 Hello
 else  suite
 Outside loop
-
-
-
-
+---------------------------------------------------------------------------------------------------------------------
+4
 Write  a  program  to  search  for  an  element  in  the  list  without  using  in  operator  and
 print  Found  or  Not  Found  message  (Assume  that  there  are  no  duplicates)
-
 Note:  Do  not  use  index()  method
 
-Let  list  be   [10 , 20 , 15 , 12 , 18]
-1) What  is  the  output  if  15  is  seacrhed ?  --->  Found  at  index  2
-
-2) What  is  the  output  if  19  is  seacrhed ?  --->  Not  found
-
-3) What  action  to  be  made  if  'x'  is  not  matched  with  the  current  element  of  list ?  --->  Compare  'x'  with  next  element  of  list
-
-4) What  action  to  be  made  if  'x'  is   matched   with  list  element ?  --->  Print  found   message  along  with  index  and don't   search  in  rest  of  the  list
-
-5) What  action  to  be  made  if  'x'  is   not  matched  with  all  the  elements  of  list ?  --->  Print  not  found   message
-6) Hint: Use  for  loop
-
-'''
 a= [10 , 20 , 15 , 12 , 18]
 x=int(input('Enter value to be searched: '))
 for i in range(len(a)):
 	if a[i]==x:
 		print('Found at index: ',i)
 		
-'''
 OP-
-Enter value to be searched: 8
-Found at index:  6
+Enter value to be searched: 10
+Found at index:  0
 
 Enter value to be searched: 9
 Not found
-
-
+-------------------------------------------------------------------------------------------------------------------------------------------
+5
 #  Walrus   operator (:=)  demo  program
 print(a := 25)   #25
 #print(a = 25)    #error
@@ -139,7 +124,8 @@ print(a)           #13
 print((a := 6) + 7)  #13
 print(a)             #6
 #print((a = 6) + 7)    #error
-
+----------------------------------------------------------------------------------------------------------
+6
 # Find  outputs  (Home  work)
 a = 0
 if  a == 0:  
@@ -154,25 +140,15 @@ else:
 #	print('Hyd')    #error because assigning 0 to c in if condition is not allowed
 #else:
 #	print('Sec')
-
-
-
+-----------------------------------------------------------------------------------------------------------
+7
 # Find  outputs
 b = 10
 a = b += 5   
 print(a)     #error- += cannot be used during assignment 
-
-
-(Home  work)
-Write  a  program to  determine  average  of  inputs  which  are  terminated  with  -1
-(without  walrus  operator)
-
-Let  inputs  be  25 , 10.8 , True ,  46 , 34.8 , False , 95 , -1
-
-sum = 0 + 25 + 10.8 + True + 46 + 34.8 + False + 95
-
-ctr = 0  + 1 + 1 + 1 + 1 + 1 + 1 + 1
-
+---------------------------------------------------------------------------------------------------
+8
+Write  a  program to  determine  average  of  inputs  which  are  terminated  with  -1 (without  walrus  operator)
 
 sum=0
 ctr=0
@@ -198,14 +174,15 @@ Enter input(-1 to stop): False
 Enter input(-1 to stop): 0
 Enter input(-1 to stop): -1
 Average:  2.4
-
-
+------------------------------------------------------------------------------------------
+9
 #  del  operator  demo program  (Home  work)
 a = 25   
 print(a) #25
 del   a   #delete reference a pointing to object 25
 #print(a)  #error-reference a is deleted
-
+-------------------------------------------------------------------------------------------------
+10
 # Find  outputs  (Home  work)
 a = b = c = 25
 print(a , b , c)  #25 25 25
@@ -217,15 +194,17 @@ print(c)          #25
 #print(b)          #error-ref b deleted 
 del   c           #delete reference c
 #print(c)          #error-ref c deleted
-
+----------------------------------------------------------------------------------------------------
+11
 #  Can  multiple  objects  be  deleted  with  same  del  operator ?
 a , b , c = 25 , 10.8 , 'Hyd'
 print(a , b , c)   #25 10.8 Hyd
 del   a , b , c    # ref are deleted
-print(a)           #error reference deleted nothing is there to print
+print(a)           #error- reference deleted nothing is there to print
 print(b)           #error
 print(c)            #error
-
+--------------------------------------------------------------------------------------------------------
+12
 # Find outputs  (Home  work)
 a = [10 , 20 , 15 , 18]
 print(a)   #[10 , 20 , 15 , 18]
@@ -234,7 +213,8 @@ print(a)   #[10 , 20 , 18]
 del  a      #delete entire list 
 print(a)    #error - list is deleted , nothing is there to print
 print(a[0]) #error
-
+--------------------------------------------------------------------------------------------------------------
+13
 # Find outputs  (Home work)
 a = (10 , 20 , 15 , 18)
 print(a)   #(10 , 20 , 15 , 18)
@@ -243,4 +223,3 @@ print(a[0]) #10
 del  a
 #print(a)    #error- ref a is deleted 
 print(a[0])  #error
-'''
