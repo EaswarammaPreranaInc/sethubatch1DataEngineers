b69132273dcb6d997a05835367e3f76c80d990a3
Gangadhar_Ganapuram <144031462+ganapuram1106@users.noreply.github.com>
2025-03-22 09:12:02 +0530

    Create gangadhar_d88_21marHW.py
    I committed 21 march homewowrk

---
+++ gangadhar_d88_21marHW.py
@@ -0,0 +1,28 @@
+# Find  outputs  (Home  work)
+def  change(b):
+	b  = [50 , 60 , 70 , 80]
+	print(b) #[50 , 60 , 70 , 80]
+# End  of  the  function
+a = [10 , 20 , 30 , 40]
+print(a)#[10 , 20 , 30 , 40]
+change(a)# [50 , 60 , 70 , 80]
+print(a)#[10 , 20 , 30 , 40]
+
+ #  Find  outputs  (Home  work)
+def   f1(x):
+	x = 20
+	print(x)
+# End  of   the   function
+x = 10
+print(x)# 10
+f1(x)#20
+print(x)# 10
+
+ #  Find  outputs  (Home  work)
+def  f1(b):
+	b[2] = 25
+#end  of  the  function
+a = (10 , 20 , 15 , 18)
+print(a)#(10 , 20 , 15 , 18)
+#f1(a)# error : b is not defined
+print(a)#(10 , 20 , 15 , 18)
