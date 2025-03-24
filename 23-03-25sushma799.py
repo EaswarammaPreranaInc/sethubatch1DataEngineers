e9307b32f18d1b76214ed7295c0f36982440dce5
sushma799 <sushmaalgubelli@gmail.com>
2025-03-24 20:43:03 +0530

    Create 23-03-25sushma799.py
    Homework program

---
+++ 23-03-25sushma799.py
@@ -0,0 +1,10 @@
+#  Find  outputs  (Home  work)
+s = 'Hyd'
+print(lambda  s  :  print(s))
+print(lambda  x  :  print(x) (s))
+print((lambda  x  :  print(x)) (s))
+(lambda  x  :  print(x)) (s)
+#output
+10, 20, 15, 18, 5]
+[5, 10, 15, 18, 20]
+[20, 18, 15, 10, 5]
