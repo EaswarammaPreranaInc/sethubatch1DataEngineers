289d50be67de9e817e14cfa9fab62574c728e8f0
Ramagirimanu <ramagirimanu@gmail.com>
2025-04-06 01:45:00 +0530

    Add files via upload

---
+++ reuse1.py
@@ -0,0 +1,16 @@
+from p1 import mod1, mod2  #How  to  import  mod1   and  mod2  of  package  p1  with  from  statement
+print(mod1.x)  #How  to  print  variable  'x'  of   mod1  in  package  p1
+mod1.f1()  #How  to  call  function  f1()  of   mod1  in  package  p1
+a = mod1.c1() #How  to  call  method  m1()  of   class  c1  in  mod1  of  package  p1
+a.m1()
+print()
+print()
+print(mod2.x)  #How  to  print  variable  'x'  of   mod2  in  package  p1
+mod2.f1()  #How  to  call  function  f1()  of   mod2  in  package  p1
+b = mod1.c1()  #How  to  call  method  m1()  of   class  c1  in  mod2  of  package  p1
+b.m1()
+#print(p1 . mod1 . x)
+#1print(x)
+
+
+
