57dd25808d95d7e0f44047fe0d57c6a2d6ba559d
priyanka-datapath <198757538+priyanka-datapath@users.noreply.github.com>
2025-04-10 18:03:26 +0530

    Update PriyankaBojja_08_04_HW.py

--- PriyankaBojja_08_04_HW.py
+++ PriyankaBojja_08_04_HW.py
@@ -89,22 +89,6 @@ Grade : Distinction
 #-----------------------------------------------------------------------------------------------------------------
 Q3 Write  a  program  to  add , subtract , multiply  and  divide  two  rational  numbers
 
-1) 1st  rational  number  --->  2 / 3
-    2nd  rational  number  --->   5 / 9
-    What  is  the  sum  ?  --->  2 / 3 + 5 / 9 = (18 + 15) / 27 =  33 / 27 =  11 / 9
-    What  is  the  difference  ?  --->  2 / 3 - 5 / 9 =  (18 - 15) / 27 =  3 / 27 =  1 / 9
-    What  is  the  product  ?  --->	2 / 3 * 5 / 9 =  10 / 27  =  10 / 27
-    What  is   the  division  ?  --->  2 / 3 /  5 / 9 =  2 / 3 * 9 / 5 =  18 / 15 =  6 / 5  --->  Successful  division
-
-2) 1st  rational  number  --->  2 / 3
-    2nd  rational  number  --->   0 / 9
-    What  is  the  sum  ?  --->  2 / 3 + 0 / 9 =  (18 + 0) / 27 =  18 / 27 =  2 / 3
-    What  is  the  difference  ?  --->  2 / 3 - 0 / 9 =  (18 - 0) / 27 =  18 / 27 = 2 / 3
-    What  is  the  product  ?  --->  2 / 3 * 0 / 9 =  0 / 27  --->  Simplification  is  not  required  becoz  numerator  is  0
-    What  is   the  division  ?  --->  2 / 3 /  0 / 9 =  2 / 3 * 9 / 0 =  18 / 0  ---> Division  is  not   permitted
-
-3) When  is  simplification  required ?  --->  When  numerator  is  non-zero
-
 import  math
 class  Rat:
 	def  get(self):
@@ -123,59 +107,29 @@ class  Rat:
 		self.nr = a.nr * b.dr + b.nr * a.dr
 		self.dr = a.dr * b.dr
 		self.simplify()
-	
-	#c . add(a , b)
-	#object  a  --->  2 / 3
-	#object  b  --->  5 / 9
-	#object  c  --->  2 / 3 + 5 / 9 = (2 * 9 + 5 * 3) / (5 * 9) = 33 / 27 = 11 / 9
-	
 	def   sub(self , a , b):
 		#How  to  subtract  objects  'a'  and  'b' and  store  results  in  object  self
 		#How  to  simplify  object  self
 		self.nr = a.nr * b.dr - b.nr * a.dr
 		self.dr = a.dr * b.dr
 		self.simplify()
-
-	#d . sub(a , b)
-	#object  a  --->  2 / 3
-	#object  b  --->  5 / 9
-	#object  d  --->  2 / 3 - 5 / 9 = (2 * 9 - 5 * 3) / (5 * 9) = 3 / 27 = 1 / 9
-	
 	def   mul(self , a , b):
 		#How  to  multiply  objects  'a'  and  'b' and  store  results  in  object  self
 		#How  to  simplify  object  self
 		self.nr = a.nr * b.nr
 		self.dr = a.dr * b.dr
 		self.simplify()
-	
-	#e . mul(a , b)
-	#object  a  --->  2 / 3
-	#object  b  --->  5 / 9
-	#object  e  --->  2 / 3 * 5 / 9 = (2 * 5) / (3 * 9) = 10 / 27
-	
 	def    div(self , a , b):
 		#How  to  divide  objects  'a'  and  'b' and  store  results  in  object  self
 		#How  to  simplify  object  self
 		self.nr = a.nr * b.dr
 		self.dr = a.dr * b.nr
 		self.simplify()
-	
-	#f . div(a , b)
-	#object  a  --->  2 / 3
-	#object  b  --->  5 / 9
-	#object  f  --->  2 / 3 / 5 / 9 = 2 / 3 * 9 / 5 = (2 * 9) / (3 * 5) = 18 / 15 = 6 / 5
-	
 	def   simplify(self):
 		if self.nr != 0:
 			gcd=math.gcd(self.nr,self.dr)  #How  to  find  gcd  of  numerator  and   denominator
 			self.nr=self.nr//gcd   #How  to  simplify  rational  number  in  object  self  i.e.  12 / 15  should  be  simplified  to  4 / 5
 			self.dr=self.dr//gcd
-	
-	#c . simplify()
-	#1)  12 / 15  --->  4 / 5
-	#2) 10 / 27   --->  10 / 27
-	#3) 0 / 27  --->   0 / 27
-
 # End  of the class
 if __name__=='__main__':
 	a=Rat()   #How  to  create  6  objects  a , b , c , d , e , f
@@ -312,5 +266,3 @@ OP-
 Empno : 25
 Ename : Rama  Rao
 Sal : 10000.0
-'''
-
