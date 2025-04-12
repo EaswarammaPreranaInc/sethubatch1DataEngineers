577939535153498b8ed5f7b129ea8759b1bc7bca
Himabindu-sh <himavummala@gmail.com>
2025-04-12 21:53:51 +0530

    Create Himabindu-shMar22.py
    Programs with outputs

---
+++ Himabindu-shMar22.py
@@ -0,0 +1,44 @@
+'''#  Find  outputs  (Home  work)
+def  change(b):
+	b . append(25)
+	b[2] = 17
+	del  b[1]
+# End  of  the  function
+a = [10 , 20 , 15 , 18]
+print(a) # [10 , 20 , 15 , 18]
+change(a) 
+print(a) # [10 , 17 , 18 , 25]
+'''
+
+'''# Find  outputs  (Home  work)
+def  change(b):
+	b  = [50 , 60 , 70 , 80]
+	print(b)
+# End  of  the  function
+a = [10 , 20 , 30 , 40]
+print(a)  #  [10 , 20 , 30 , 40]
+change(a) # [50 , 60 , 70 , 80]
+print(a)  # [10 , 20 , 30 , 40]
+'''
+
+'''#  Find  outputs  (Home  work)
+def   f1(x):
+	x = 20
+	print(x)
+# End  of   the   function
+x = 10
+print(x) # 10
+f1(x)  # 20
+print(x) # 10
+'''
+
+'''#  Find  outputs  (Home  work)
+def  f1(b):
+	#b[2] = 25
+	pass
+#end  of  the  function
+a = (10 , 20 , 15 , 18)
+print(a) #  (10 , 20 , 15 , 18)
+f1(a)   # error tuple can not be modified because it is immutable
+print(a) # (10 , 20 , 15 , 18)
+'''  
